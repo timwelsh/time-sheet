8c57d976c1e2acd845c3714001c1ffaf51a3ff83
Tim Welsh <tim@timwelsh.com>
2019-01-26 12:43:22 -0500

    Add Toms changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,59 @@
-$(document).ready(function() {
-      // Initialize Firebase
-  var config = {
+$( document ).ready(function() {
+
+    var config = {
     apiKey: "AIzaSyDkG0vT9-BRgAl409N4OIkU82O8BG3i1ow",
     authDomain: "timesheet-6050c.firebaseapp.com",
     databaseURL: "https://timesheet-6050c.firebaseio.com",
     projectId: "timesheet-6050c",
     storageBucket: "",
     messagingSenderId: "653578073352"
-  };
-  firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
 
-  // Create a variable to reference the database
-  var database = firebase.database();
+    // Create a variable to reference the database
+    var database = firebase.database();
 
-  var employee;
-  var startDate;
-  var role;
-  var rate;
+    var employee;
+    var startDate;
+    var role;
+    var rate;
 
-  $("#add-employee").on("click", function(event) {
-    event.preventDefault();
 
-    employee = $("#employee-input").val().trim();
-    startDate = $("#startdate-input").val().trim();
-    role = $("#role-input").val().trim();
-    rate = $("#rate-input").val().trim();
+    database.ref().on("child_added", function(snapshot) {
 
-    database.ref().push({
-        employee: employee,
-        startDate: startDate,
-        role: role,
-        rate: rate,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-  }
+        employee = snapshot.val().employee;
+        role = snapshot.val().role;
+        startDate = snapshot.val().startDate;
+        rate = snapshot.val().rate;
 
-      // Firebase watcher .on("child_added"
-      database.ref().on("child_added", function(snapshot) {
-        // storing the snapshot.val() in a variable for convenience
-        var emp = snapshot.val();
-  
-        // Change the HTML to reflect
-        $("#employee-display").append(emp.employee);
-        $("#startdate-display").append(emp.startDate);
-        $("#role-display").append(emp.role);
-        $("#rate-display").append(emp.rate);
-  
-        // Handle the errors
-      }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      });
-}
+
+        $("#employee-display").append(employee + "<br />");
+        $("#startdate-display").append(startDate + "<br />");
+        $("#role-display").append(role + "<br />");
+        $("#rate-display").append(rate + "<br />");
+        $("#months-display").append(months + "<br />");
+        $("#billed-display").append(billed + "<br />");
+    
+        
+    }, function(errorObject) {
+    console.log("Error: " + errorObject.code);
+    });
+
+    $('#add-employee').click(function(event) {
+        event.preventDefault()
+
+        employee = $("#employee-input").val().trim();
+        startDate = $("#startDate-input").val().trim();
+        role = $("#role-input").val().trim();
+        rate = $("#rate-input").val().trim();
+
+        database.ref().push({
+            employee : employee,
+            role: role,
+            startDate: startDate,
+            rate: rate,
+
+        })
+
+    });
+});
